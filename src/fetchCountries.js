f71e630513fb285fd895f749ce553521e262d237
Yurii Panasiuk <yuriipanasiuk@gmail.com>
2022-08-26 18:41:23 +0300

    change fetch

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,10 +1,8 @@
-const MAIN_URL = `https://restcountries.com/v3.1/name`;
+const BASE_URL = `https://restcountries.com/v3.1/name`;
 const FILTER_RESPONSE = `fields=name,capital,population,flags,languages`;
 
 export function fetchCountries(name) {
-  if (name) {
-    return fetch(`${MAIN_URL}/${name}?${FILTER_RESPONSE}`).then(onSuccessFetch);
-  }
+  return fetch(`${BASE_URL}/${name}?${FILTER_RESPONSE}`).then(onSuccessFetch);
 }
 
 function onSuccessFetch(response) {
